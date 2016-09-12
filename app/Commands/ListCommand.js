130e8bc08379d6e43cb83555c275bf93649c6fd4
shnhrrsn <shnhrrsn@gmail.com>
2016-09-12 00:53:38 -0400

    Fixed list command

--- app/Commands/ListCommand.js
+++ app/Commands/ListCommand.js
@@ -2,27 +2,16 @@ import 'App/Commands/BaseCommand'
 import 'App/Support/FS'
 
 export class ListCommand extends BaseCommand {
-	// Name of the command
 	name = 'list'
-
-	// Description of the command to show in help
-	description = 'Command description'
-
-	// Arguments available for this command
-	// arguments = [ 'requiredArgument', 'optionalArgument?' ]
-
-	// Options for this command
-	// options = {
-	// 	someOption: 'Description of this option'
-	// }
+	description = 'Lists all registered domains.'
 
 	async run() {
 		const files = await FS.readdir(this.app.paths.hosts())
 
 		for(const file of files) {
 			const config = await FS.readFile(this.app.paths.hosts(file)).then(contents => contents.toString())
-			const target = config.match(/#target:(.+?)$/m)[1].trim()
-			this.comment(`${file.replace(/\.conf/, '')} => ${target}`)
+			const proxy = ((config.match(/#proxy:(.+?)$/m) || [ ])[1] || '???').trim()
+			this.comment(`${file.replace(/\.conf$/, '')} => ${proxy}`)
 		}
 	}
 
