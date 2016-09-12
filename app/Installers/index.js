d662ee2795144163b6e5f0e2059efaa8684a6b8a
shnhrrsn <shnhrrsn@gmail.com>
2016-09-12 00:43:36 -0400

    Changed Caddy to run on 80

--- app/Installers/index.js
+++ app/Installers/index.js
@@ -1,12 +1,9 @@
 import 'App/Installers/CaddyInstaller'
 import 'App/Installers/DnsmasqInstaller'
 import 'App/Installers/NgrokInstaller'
-import 'App/Installers/PortForwardingInstaller'
 
 export const Installers = [
 	DnsmasqInstaller,
-	PortForwardingInstaller,
-
 	CaddyInstaller,
 	NgrokInstaller,
 ]
