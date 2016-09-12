6c4a35eb67e896c4fd8e50d888168b6ac45562fe
shnhrrsn <shnhrrsn@gmail.com>
2016-09-12 00:52:55 -0400

    Build out undock command to unregister projects with Caddy

---
+++ app/Commands/UndockCommand.js
@@ -0,0 +1,27 @@
+import 'App/Commands/BaseDockCommand'
+import 'App/Support/FS'
+
+export class UndockCommand extends BaseDockCommand {
+	name = 'undock'
+	description = 'Unregister the a project with Marina'
+	options = {
+		domain: [ 'Domain', 'string' ]
+	}
+
+	async run() {
+		const exists = await FS.exists(this.configPath)
+
+		if(!exists) {
+			this.error('--> This domain does not exist.')
+			process.exit(1)
+		}
+
+		await FS.unlink(this.configPath)
+
+		this.comment('Restarting')
+		await this.caddy.restart()
+
+		this.success('Done')
+	}
+
+}
--- app/Providers/CommandsProvider.js
+++ app/Providers/CommandsProvider.js
@@ -1,11 +1,13 @@
 import 'App/Commands/DockCommand'
 import 'App/Commands/InstallCommand'
 import 'App/Commands/ListCommand'
+import 'App/Commands/UndockCommand'
 import 'App/Commands/UninstallCommand'
 
 export function CommandsProvider(app) {
 	app.cli.register(DockCommand)
 	app.cli.register(InstallCommand)
 	app.cli.register(ListCommand)
+	app.cli.register(UndockCommand)
 	app.cli.register(UninstallCommand)
 }
