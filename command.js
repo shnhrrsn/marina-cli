14e22d44766f30f8dd04e95d679cc31305c4fc42
shnhrrsn <shnhrrsn@gmail.com>
2017-03-26 13:07:22 -0400

    Updated to use native async/await when running in node 7

--- command.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-const path = require('path')
-
-process.env.ORIGINAL_WD = process.cwd()
-process.chdir(path.join(__dirname, 'lib'))
-
-require(path.join(__dirname, 'lib/boot/Cli.js'))
