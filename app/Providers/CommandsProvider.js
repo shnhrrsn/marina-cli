60bff397687ef366b5727b2465757d61eba30488
shnhrrsn <shnhrrsn@gmail.com>
2017-03-26 13:49:17 -0400

    Only the `install` command is now available until it’s ran

--- app/Providers/CommandsProvider.js
+++ app/Providers/CommandsProvider.js
@@ -1,3 +1,5 @@
+import { FS } from 'grind-support'
+
 import 'App/Commands/DockCommand'
 import 'App/Commands/InstallCommand'
 import 'App/Commands/ListCommand'
@@ -8,15 +10,18 @@ import 'App/Commands/StopCommand'
 import 'App/Commands/UndockCommand'
 import 'App/Commands/UninstallCommand'
 
-export function CommandsProvider(app) {
+export async function CommandsProvider(app) {
 	app.cli.commands.length = 0
-	app.cli.register(DockCommand)
 	app.cli.register(InstallCommand)
-	app.cli.register(ListCommand)
-	app.cli.register(RestartCommand)
-	app.cli.register(ShareCommand)
-	app.cli.register(StartCommand)
-	app.cli.register(StopCommand)
-	app.cli.register(UndockCommand)
 	app.cli.register(UninstallCommand)
+
+	if(await FS.exists(app.paths.home('Caddyfile'))) {
+		app.cli.register(DockCommand)
+		app.cli.register(ListCommand)
+		app.cli.register(RestartCommand)
+		app.cli.register(ShareCommand)
+		app.cli.register(StartCommand)
+		app.cli.register(StopCommand)
+		app.cli.register(UndockCommand)
+	}
 }
