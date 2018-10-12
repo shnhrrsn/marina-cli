63573426954ee4a1222f309bbf0f121be2f896d6
shnhrrsn <shnhrrsn@gmail.com>
2018-10-12 16:44:18 -0400

    Added prettier output for `list`

--- app/Commands/ListCommand.js
+++ app/Commands/ListCommand.js
@@ -1,19 +1,48 @@
 import 'App/Commands/BaseCommand'
 import 'App/Support/Site'
 
+const chalk = require('chalk')
+
 export class ListCommand extends BaseCommand {
 
 	name = 'list'
 	description = 'Lists all registered domains.'
 
-	run() {
-		return Site.forEach(this.app, (file, site) => {
-			this.info(`${site.fqdn} => ${site.proxy}`)
+	async run() {
+		this.info('')
+		const sites = [ ]
 
-			for(const alias of site.fqAliases) {
-				this.info(`${alias} => ${site.proxy}`)
-			}
+		await Site.forEach(this.app, (file, site) => {
+			sites.push(site)
 		})
+
+		const max = Math.max(...sites.map(site => site.fqdn.length)) + 3
+
+		for(const site of sites) {
+			let line = '  '
+			line += chalk.green(`http://${site.fqdn.padEnd(max)}`)
+			line += chalk.gray('  =>  ')
+			line += chalk.blue(site.source)
+
+			if(site.hasAliases) {
+				let alias = ''
+
+				if(site.aliases.length === 1) {
+					alias += ' [Alias: '
+				} else {
+					alias += ' [Aliases: '
+				}
+
+				alias += site.fqAliases.map(a => chalk.yellow(`http://${a}`)).join(', ')
+				alias += ']'
+
+				line += chalk.gray(alias)
+			}
+
+			this.info(line)
+		}
+
+		this.info('')
 	}
 
 }
